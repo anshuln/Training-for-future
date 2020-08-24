438de8b07bae69a369cd8bba13e79760c6a03969
anshuln <anshulnasery@gmail.com>
2020-08-24 23:38:45 +0530

    Adds wasserstein disc

--- codes/torch_trans_disc/models.py
+++ codes/torch_trans_disc/models.py
@@ -7,98 +7,139 @@ from torch import nn
 import torch
 
 class ClassifyNet(nn.Module):
-	def __init__(self,data_shape, hidden_shape, out_shape, time_conditioning=False):
-		super(ClassifyNet,self).__init__()
+    def __init__(self,data_shape, hidden_shape, out_shape, time_conditioning=True):
+        super(ClassifyNet,self).__init__()
 
-		self.time_conditioning = time_conditioning
+        self.time_conditioning = time_conditioning
 
-		self.layer_0 = nn.Linear(data_shape,hidden_shape)
-		self.relu    = nn.ReLU()
+        self.layer_0 = nn.Linear(data_shape,hidden_shape)
+        self.relu    = nn.LeakyReLU()
 
-		if time_conditioning:
-			self.layer_1 = nn.Linear(hidden_shape,hidden_shape)
-			self.layer_2 = nn.Linear(hidden_shape,hidden_shape)
+        if time_conditioning:
+            self.layer_1 = nn.Linear(hidden_shape,hidden_shape)
+            self.layer_2 = nn.Linear(hidden_shape,hidden_shape)
 
-		self.out_layer = nn.Linear(hidden_shape,out_shape)
+        self.out_layer = nn.Linear(hidden_shape,out_shape)
 
-	def forward(self,X):
-		X  = self.layer_0(X)
-		X  = self.relu(X)
+    def forward(self,X):
+        X  = self.layer_0(X)
+        X  = self.relu(X)
 
-		if self.time_conditioning:
-			X = self.layer_1(X)
-			X = self.relu(X)
-			X = self.layer_2(X)
-			X = self.relu(X)
+        if self.time_conditioning:
+            X = self.layer_1(X)
+            X = self.relu(X)
+            X = self.layer_2(X)
+            X = self.relu(X)
 
-		X = self.out_layer(X)
-		X = self.relu(X)
-		X = torch.softmax(X,dim=1)
+        X = self.out_layer(X)
+        X = self.relu(X)
+        X = torch.softmax(X,dim=1)
 
-		return X
+        return X
 
 
 class Transformer(nn.Module):
-	def __init__(self,data_shape, latent_shape):
+    def __init__(self,data_shape, latent_shape, label_dim=0):
+        '''
+        Notes to self - 
+            1. This should transform both X and Y
+            2. We can leverage joint modelling of X and Y somehow?
+            3. Are we taking on a tougher task? Will it work to just time travel one unit?
+            4. Can we decouple the training process of generating X and Y?
+        Seems to collapse domain somehow, maybe network power issue? 
+        Classifier seems to aggravate it, but classifier loss is "most potent" on this dataset
+        Further, labels seem right when classifier loss is added
+        Also, is extrapolating good?
+        '''
+        super(Transformer,self).__init__()
+        self.layer_0 = nn.Linear(data_shape,latent_shape)
+        self.layer_0_0 = nn.Linear(latent_shape,latent_shape)
+        # self.layer_0_1 = nn.Linear(latent_shape,latent_shape)
+        # self.layer_0_2 = nn.Linear(latent_shape,latent_shape)
 
-		super(Transformer,self).__init__()
-		self.layer_0 = nn.Linear(data_shape,latent_shape)
-		self.layer_1 = nn.Linear(latent_shape,data_shape-2)
-		self.leaky_relu = nn.LeakyReLU()
+        self.layer_1 = nn.Linear(latent_shape,data_shape-2)
+        self.leaky_relu = nn.LeakyReLU()
+        self.label_dim = label_dim
 
-	def forward(self,X):
-		X = self.leaky_relu(self.layer_1(self.leaky_relu(self.layer_0(X))))
-		return X
+    def forward(self,X):
+        X = (self.layer_1((self.leaky_relu(self.layer_0_0(self.leaky_relu(self.layer_0(X)))))))
+        if self.label_dim:
+            lab = torch.sigmoid(X[:,-1])
+            # X_new = self.leaky_relu(X[:,:-1])
+            X = torch.cat([X,lab.unsqueeze(1)],dim=1)
+        # else:
+        #     X = self.leaky_relu(X)
+        return X
 
 
 class Discriminator(nn.Module):
-	def __init__(self,data_shape, hidden_shape, is_wasserstein=False):
+    def __init__(self,data_shape, hidden_shape, is_wasserstein=False):
 
-		super(Discriminator,self).__init__()
-		self.layer_0   = nn.Linear(data_shape,hidden_shape)
-		self.relu      = nn.ReLU()
-		self.out_layer = nn.Linear(hidden_shape,1)
-		self.is_wasserstein = is_wasserstein
+        super(Discriminator,self).__init__()
+        self.layer_0   = nn.Linear(data_shape,hidden_shape)
+        self.relu      = nn.LeakyReLU()
+        self.out_layer = nn.Linear(hidden_shape,1)
+        self.is_wasserstein = is_wasserstein
 
-	def forward(self,X):
-		X = self.out_layer(self.relu(self.layer_0(X)))
+    def forward(self,X):
+        X = self.out_layer(self.relu(self.layer_0(X)))
 
-		if not self.is_wasserstein:
-			X = torch.sigmoid(X)
-			
-		return X
+        if not self.is_wasserstein:
+            X = torch.sigmoid(X)
+            
+        return X
 
 
 def classification_loss(Y_pred, Y):
-	return  -1.*torch.sum((Y * torch.log(Y_pred)))
+    # print(Y_pred)
+    return  -1.*torch.sum((Y * torch.log(Y_pred+ 1e-5)))
 
 def bxe(real, fake):
-	return -1.*((real*torch.log(fake)) + ((1-real)*torch.log(1-fake)))
+    return -1.*((real*torch.log(fake+ 1e-5)) + ((1-real)*torch.log(1-fake + 1e-5)))
 
 def discriminator_loss(real_output, trans_output):
 
-	# bxe = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+    # bxe = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 
-	real_loss = bxe(torch.ones_like(real_output), real_output)
-	trans_loss = bxe(torch.zeros_like(trans_output), trans_output)
-	total_loss = real_loss + trans_loss
-	
-	return total_loss.mean()
+    real_loss = bxe(torch.ones_like(real_output), real_output)
+    trans_loss = bxe(torch.zeros_like(trans_output), trans_output)
+    total_loss = real_loss + trans_loss
+    
+    return total_loss.mean()
+def discriminator_loss_wasserstein(real_output, trans_output):
+
+    # bxe = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+    real_loss = torch.mean(real_output)
+    trans_loss = -torch.mean(trans_output)
+    total_loss = real_loss + trans_loss
+    
+    return total_loss
 
 def reconstruction_loss(x,y):
-	return torch.sum((x-y)**2,dim=1)
+    # print(torch.cat([x,y],dim=1))
+    return torch.sum((x-y)**2,dim=1)
 
 
-def transformer_loss(trans_output):
+def transformer_loss(trans_output,is_wasserstein=False):
 
-	return bxe(torch.ones_like(trans_output), trans_output)
+    if is_wasserstein:
+        return trans_output
+    return bxe(torch.ones_like(trans_output), trans_output)
 
-def discounted_transformer_loss(real_data, trans_data, trans_output):
+def discounted_transformer_loss(real_data, trans_data, trans_output, pred_class, actual_class,is_wasserstein=False):
 
-	time_diff = torch.exp(-(real_data[:,-1] - real_data[:,-2]))
-	re_loss = reconstruction_loss(real_data[:,0:-2], trans_data)
-	tr_loss = transformer_loss(trans_output)
+    time_diff = torch.exp(-(real_data[:,-1] - real_data[:,-2]))
 
-	loss = torch.mean(time_diff * tr_loss + (1-time_diff) * re_loss)
-	return loss
+
+    re_loss = reconstruction_loss(real_data[:,0:-2], trans_data)
+    tr_loss = transformer_loss(trans_output,is_wasserstein)
+    # transformed_class = trans_data[:,-1].view(-1,1)
+
+    # print(actual_class,pred_class)
+    class_loss = bxe(actual_class,pred_class)
+    
+    loss = torch.mean(time_diff * tr_loss + ((1-time_diff) * re_loss) + 0.5*class_loss)
+    # loss = tr_loss.mean()
+    return loss, tr_loss.mean(),re_loss.mean(), class_loss.mean()
 
--- codes/torch_trans_disc/train.py
+++ codes/torch_trans_disc/train.py
@@ -9,28 +9,53 @@ from sklearn.datasets import make_classification, make_moons
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from models import *
 from data_loaders import *
+from torch.utils.tensorboard import SummaryWriter
+import time 
 
+def train_transformer_batch(X,Y,transformer,discriminator,classifier,transformer_optimizer,is_wasserstein=False):
 
-def train_transformer_batch(X,transformer,discriminator,transformer_optimizer):
-
+    transformer_optimizer.zero_grad()
     X_pred = transformer(X)
     domain_info = X[:,-1].view(-1,1)
     X_pred_domain_info = torch.cat([X_pred, domain_info], dim=1)
 
     is_real = discriminator(X_pred_domain_info)
 
-    trans_loss = discounted_transformer_loss(X, X_pred, is_real)
+    X_pred_class = torch.cat([X_pred,domain_info],dim=1)
+    pred_class = classifier(X_pred_class)
+
+    trans_loss,ld,lr, lc = discounted_transformer_loss(X, X_pred, is_real, pred_class[:,0].view(-1,1),Y[:,0].view(-1,1),is_wasserstein)
 
     # gradients_of_transformer = trans_tape.gradient(trans_loss, transformer.trainable_variables)
     trans_loss.backward()
 
     transformer_optimizer.step()
 
-    return trans_loss
+    return trans_loss, ld, lr, lc
+
+
+def train_discriminator_batch_wasserstein(X_old, X_now, transformer, discriminator, discriminator_optimizer):
+    
+    discriminator_optimizer.zero_grad()
+    X_pred_old = transformer(X_old)
+    domain_info = X_old[:,-1].view(-1,1)
+    X_pred_old_domain_info = torch.cat([X_pred_old, domain_info], dim=1)
+
+    is_real_old = discriminator(X_pred_old_domain_info)
+    is_real_now = discriminator(X_now[:,0:-1])
+    
+    disc_loss = discriminator_loss_wasserstein(is_real_now, is_real_old)
+
+    disc_loss.backward()
+
+    discriminator_optimizer.step()
+    for p in discriminator.parameters():
+        p.data.clamp_(-0.5, 0.5)
+    return disc_loss
 
 def train_discriminator_batch(X_old, X_now, transformer, discriminator, discriminator_optimizer):
 
-
+    discriminator_optimizer.zero_grad()
     X_pred_old = transformer(X_old)
     domain_info = X_old[:,-1].view(-1,1)
     X_pred_old_domain_info = torch.cat([X_pred_old, domain_info], dim=1)
@@ -48,7 +73,7 @@ def train_discriminator_batch(X_old, X_now, transformer, discriminator, discrimi
 
 def train_classifier(X, Y, classifier, transformer, classifier_optimizer):
 
-
+    classifier_optimizer.zero_grad()
     X_pred = transformer(X)
     domain_info = tf.reshape(X[:,-2], [-1,1])
     X_pred_domain_info = tf.cat([X_pred, domain_info], axis=1)
@@ -65,12 +90,14 @@ def train_classifier(X, Y, classifier, transformer, classifier_optimizer):
 
 def train_classifier_d(X, Y, classifier, classifier_optimizer,verbose=False):
 
-    Y_pred = classifier(X[:,0:-1])
+    classifier_optimizer.zero_grad()
+    Y_pred = classifier(X)
     pred_loss = classification_loss(Y_pred, Y)/BATCH_SIZE
     # pred_loss = pred_loss.sum()
     pred_loss.backward()
+    
     if verbose:
-	    # print(torch.cat([Y_pred, Y, Y*torch.log(Y_pred),
+        # print(torch.cat([Y_pred, Y, Y*torch.log(Y_pred),
         # (Y*torch.log(Y_pred)).sum().unsqueeze(0).unsqueeze(0).repeat(BATCH_SIZE,1) ],dim=1))
         for p in classifier.parameters():
             print(p.data)
@@ -82,10 +109,12 @@ def train_classifier_d(X, Y, classifier, classifier_optimizer,verbose=False):
 
 
 EPOCH = 500
+CLASSIFIER_EPOCHS = 250
 SUBEPOCH = 10
 BATCH_SIZE = 64
 DISC_BATCH_SIZE=64
 SHUFFLE_BUFFER_SIZE=4096
+IS_WASSERSTEIN = True
 
 def train(X_data, Y_data, U_data, num_indices, source_indices, target_indices):
 
@@ -100,18 +129,41 @@ def train(X_data, Y_data, U_data, num_indices, source_indices, target_indices):
     U_target = U_data[target_indices]
 
 
-    transformer = Transformer(4, 3)
-    discriminator = Discriminator(3, 3)
+    transformer = Transformer(4, 4)
+    discriminator = Discriminator(3, 3,IS_WASSERSTEIN)
     classifier = ClassifyNet(3,3,2)
 
-    transformer_optimizer   = torch.optim.Adagrad(transformer.parameters(),5e-2)
-    classifier_optimizer    = torch.optim.Adagrad(classifier.parameters(),5e-3)
-    discriminator_optimizer = torch.optim.Adagrad(discriminator.parameters(),5e-2)
+    transformer_optimizer   = torch.optim.Adagrad(transformer.parameters(),1e-2)
+    classifier_optimizer    = torch.optim.Adagrad(classifier.parameters(),5e-2)
+    discriminator_optimizer = torch.optim.Adagrad(discriminator.parameters(),5e-3)
 
     X_past = X_source[0]
     U_past = U_source[0]
     Y_past = Y_source[0]
-    
+    writer = SummaryWriter(comment='{}'.format(time.time()))
+
+    for class_index in range(1,len(X_source)):
+        X_past = np.vstack([X_past, X_source[class_index]])
+        Y_past = np.vstack([Y_past, Y_source[class_index]])
+        U_past = np.hstack([U_past, U_source[class_index]])     
+    print("-------------TRAINING CLASSIFIER----------")
+    class_step = 0
+    for epoch in range(CLASSIFIER_EPOCHS):
+        past_dataset = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(torch.tensor(X_past).float(),torch.tensor(U_past).float(), torch.tensor(Y_past).float()),BATCH_SIZE,True)
+        class_loss = 0
+        for batch_X, batch_U, batch_Y in past_dataset:
+
+            batch_X = torch.cat([batch_X,batch_U.view(-1,1)],dim=1)
+
+            l = train_classifier_d(batch_X,batch_Y,classifier,classifier_optimizer,verbose=False)
+            class_step += 1
+            class_loss += l
+        print("Epoch %d Loss %f"%(epoch,class_loss),flush=False)
+
+    X_past = X_source[0]
+    U_past = U_source[0]
+    Y_past = Y_source[0]
+
     for index in range(1, len(X_source)):
 
         print('Domain %d' %index)
@@ -128,11 +180,12 @@ def train(X_data, Y_data, U_data, num_indices, source_indices, target_indices):
         U_past = np.hstack([U_past, U_source[index]])
         
         all_dataset = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(torch.tensor(X_past).float(), torch.tensor(U_past).float(), torch.tensor(Y_past).float()),BATCH_SIZE,True)
-
+        step_t = 0
+        step_d = 0
+        step_c = 0
         for epoch in range(EPOCH):
 
             loss1, loss2 = 0,0
-            
             for batch_X, batch_U, batch_Y in all_dataset:
 
                 # batch_X = tf.cast(batch_X, dtype=tf.float32)
@@ -143,10 +196,13 @@ def train(X_data, Y_data, U_data, num_indices, source_indices, target_indices):
                 this_U = torch.tensor([U_source[index][0]]*batch_U.shape[0]).float()
                 this_U = this_U.view(-1,1)
                 batch_X = torch.cat([batch_X, batch_U, this_U], dim=1)
-                
-                loss1 += train_transformer_batch(batch_X,transformer,discriminator,transformer_optimizer) #train_transformer_batch(batch_X)
-
-
+                loss_t,ltd,lr,lc = train_transformer_batch(batch_X,batch_Y,transformer,discriminator,classifier,transformer_optimizer,is_wasserstein=IS_WASSERSTEIN) #train_transformer_batch(batch_X)
+                loss1 += loss_t
+                writer.add_scalar('Loss/transformer',loss_t.detach().numpy(),step_t)
+                writer.add_scalar('Loss/transformer_disc',ltd.detach().numpy(),step_t)
+                writer.add_scalar('Loss/transformer_rec',lr.detach().numpy(),step_t)
+                writer.add_scalar('Loss/transformer_classifier',lc.detach().numpy(),step_t)
+                step_t += 1
             for batch_X, batch_U, batch_Y in past_dataset:
 
                 # batch_X = tf.cast(batch_X, dtype=tf.float32)
@@ -171,77 +227,82 @@ def train(X_data, Y_data, U_data, num_indices, source_indices, target_indices):
                             U_source[index][indices].reshape(-1,1)])
 
                 real_X = torch.tensor(real_X).float()
-                loss2 += train_discriminator_batch(batch_X, real_X, transformer, discriminator, discriminator_optimizer) #train_discriminator_batch(batch_X, real_X)
-
+                if IS_WASSERSTEIN:
+                    loss_d = train_discriminator_batch_wasserstein(batch_X, real_X, transformer, discriminator, discriminator_optimizer) #train_discriminator_batch(batch_X, real_X)
+                else:
+                    loss_d = train_discriminator_batch(batch_X, real_X, transformer, discriminator, discriminator_optimizer)
+                loss2 += loss_d
+                writer.add_scalar('Loss/disc',loss_d.detach().numpy(),step_d)
+                step_d += 1
             print('Epoch %d - %f, %f' % (epoch, loss1.detach().cpu().numpy(), loss2.detach().cpu().numpy()))
 
     
-    for i in range(len(X_target)):
-        target_dataset = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(torch.tensor(X_target[i]).float(), torch.tensor(U_target[i]).float(), torch.tensor(Y_target[i]).float()),BATCH_SIZE,True)
-        source_dataset = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(torch.tensor(X_past).float(), torch.tensor(U_past).float(), torch.tensor(Y_past).float()),BATCH_SIZE,True)
+    # for i in range(len(X_target)):
+    #     target_dataset = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(torch.tensor(X_target[i]).float(), torch.tensor(U_target[i]).float(), torch.tensor(Y_target[i]).float()),BATCH_SIZE,True)
+    #     source_dataset = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(torch.tensor(X_past).float(), torch.tensor(U_past).float(), torch.tensor(Y_past).float()),BATCH_SIZE,True)
 
 
-        step = 0
-        for epoch in range(EPOCH):
+    #     step = 0
+    #     for epoch in range(EPOCH):
 
-            loss = 0
+    #         loss = 0
             
-            for batch_X, batch_U, batch_Y in source_dataset:
-                # print("Training")
+    #         for batch_X, batch_U, batch_Y in source_dataset:
+    #             # print("Training")
 
-                # batch_X = tf.cast(batch_X, dtype=tf.float32)
-                # batch_Y = tf.cast(batch_Y, dtype=tf.float32)
-                # batch_U = tf.cast(batch_U, dtype=tf.float32)
+    #             # batch_X = tf.cast(batch_X, dtype=tf.float32)
+    #             # batch_Y = tf.cast(batch_Y, dtype=tf.float32)
+    #             # batch_U = tf.cast(batch_U, dtype=tf.float32)
 
-                # batch_U = tf.reshape(batch_U, [-1,1])
-                # this_U = tf.constant([U_target[i][0]]*batch_U.shape[0], dtype=tf.float32)
-                # this_U = tf.reshape(this_U, [-1,1])
-                # batch_X = tf.cat([batch_X, batch_U, this_U], axis=1)
-                batch_U = batch_U.view(-1,1)
-                this_U = torch.tensor([U_source[index][0]]*batch_U.shape[0]).float()
-                this_U = this_U.view(-1,1)
-                batch_X = torch.cat([batch_X, batch_U, this_U], dim=1)
-                step += 1
-                loss += train_classifier_d(batch_X, batch_Y, classifier, classifier_optimizer, verbose=False)
+    #             # batch_U = tf.reshape(batch_U, [-1,1])
+    #             # this_U = tf.constant([U_target[i][0]]*batch_U.shape[0], dtype=tf.float32)
+    #             # this_U = tf.reshape(this_U, [-1,1])
+    #             # batch_X = tf.cat([batch_X, batch_U, this_U], axis=1)
+    #             batch_U = batch_U.view(-1,1)
+    #             this_U = torch.tensor([U_source[index][0]]*batch_U.shape[0]).float()
+    #             this_U = this_U.view(-1,1)
+    #             batch_X = torch.cat([batch_X, batch_U, this_U], dim=1)
+    #             step += 1
+    #             loss += train_classifier_d(batch_X, batch_Y, classifier, classifier_optimizer, verbose=False)
 
-            # target_dataset = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(torch.tensor(X_target[i]).float(), torch.tensor(U_target[i]).float(), torch.tensor(Y_target[i]).float()),BATCH_SIZE,True)
-            # source_dataset = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(torch.tensor(X_past).float(), torch.tensor(U_past).float(), torch.tensor(Y_past).float()),BATCH_SIZE,True)
-               # print("%f" % loss)
-            print('Epoch: %d - ClassificationLoss: %f' % (epoch, loss))
+    #         # target_dataset = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(torch.tensor(X_target[i]).float(), torch.tensor(U_target[i]).float(), torch.tensor(Y_target[i]).float()),BATCH_SIZE,True)
+    #         # source_dataset = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(torch.tensor(X_past).float(), torch.tensor(U_past).float(), torch.tensor(Y_past).float()),BATCH_SIZE,True)
+    #            # print("%f" % loss)
+    #         print('Epoch: %d - ClassificationLoss: %f' % (epoch, loss))
 
         
-        #print(classifier.trainable_variables)
-        Y_pred = []
-        for batch_X, batch_U, batch_Y in target_dataset:
+    #     #print(classifier.trainable_variables)
+    #     Y_pred = []
+    #     for batch_X, batch_U, batch_Y in target_dataset:
 
-            # batch_X = tf.cast(batch_X, dtype=tf.float32)
-            # batch_Y = tf.cast(batch_Y, dtype=tf.float32)
-            # batch_U = tf.cast(batch_U, dtype=tf.float32)
+    #         # batch_X = tf.cast(batch_X, dtype=tf.float32)
+    #         # batch_Y = tf.cast(batch_Y, dtype=tf.float32)
+    #         # batch_U = tf.cast(batch_U, dtype=tf.float32)
 
-            # batch_U = tf.reshape(batch_U, [-1,1])
-            # this_U = tf.constant([U_target[i][0]]*batch_U.shape[0], dtype=tf.float32)
-            # this_U = tf.reshape(this_U, [-1,1])
-            # batch_X = tf.cat([batch_X, batch_U, this_U], axis=1)
+    #         # batch_U = tf.reshape(batch_U, [-1,1])
+    #         # this_U = tf.constant([U_target[i][0]]*batch_U.shape[0], dtype=tf.float32)
+    #         # this_U = tf.reshape(this_U, [-1,1])
+    #         # batch_X = tf.cat([batch_X, batch_U, this_U], axis=1)
 
-            batch_U = batch_U.view(-1,1)
-            this_U = torch.tensor([U_source[index][0]]*batch_U.shape[0]).float()
-            this_U = this_U.view(-1,1)
-            batch_X = torch.cat([batch_X, batch_U, this_U], dim=1)
-            #batch_X_pred = transformer(batch_X)
-            #domain_info = tf.reshape(batch_X[:,-2], [-1,1])
-            #X_pred_domain_info = tf.cat([batch_X_pred, domain_info], axis=1)
-            batch_Y_pred = classifier(batch_X[:,0:-1]).detach().cpu().numpy()
+    #         batch_U = batch_U.view(-1,1)
+    #         this_U = torch.tensor([U_source[index][0]]*batch_U.shape[0]).float()
+    #         this_U = this_U.view(-1,1)
+    #         batch_X = torch.cat([batch_X, batch_U, this_U], dim=1)
+    #         #batch_X_pred = transformer(batch_X)
+    #         #domain_info = tf.reshape(batch_X[:,-2], [-1,1])
+    #         #X_pred_domain_info = tf.cat([batch_X_pred, domain_info], axis=1)
+    #         batch_Y_pred = classifier(batch_X[:,0:-1]).detach().cpu().numpy()
 
-            Y_pred = Y_pred + [batch_Y_pred]
+    #         Y_pred = Y_pred + [batch_Y_pred]
 
-        Y_pred = np.vstack(Y_pred)
-        print('shape: ',Y_pred.shape)
-        print(Y_pred)
-        Y_pred = np.array([0 if y[0] > y[1] else 1 for y in Y_pred])
-        Y_true = np.array([0 if y[0] > y[1] else 1 for y in Y_target[i]])
-        print(accuracy_score(Y_true, Y_pred))
-        print(confusion_matrix(Y_true, Y_pred))
-        print(classification_report(Y_true, Y_pred))    
+    #     Y_pred = np.vstack(Y_pred)
+    #     print('shape: ',Y_pred.shape)
+    #     print(Y_pred)
+    #     Y_pred = np.array([0 if y[0] > y[1] else 1 for y in Y_pred])
+    #     Y_true = np.array([0 if y[0] > y[1] else 1 for y in Y_target[i]])
+    #     print(accuracy_score(Y_true, Y_pred))
+    #     print(confusion_matrix(Y_true, Y_pred))
+    #     print(classification_report(Y_true, Y_pred))    
     return transformer,discriminator,classifier
 
 
