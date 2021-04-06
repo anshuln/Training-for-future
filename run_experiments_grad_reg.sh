7c97b52a3a31285cbea19b8564c0f45d5f502558
Anshul Nasery <38972131+anshuln@users.noreply.github.com>
2021-04-06 12:44:02 +0530

    Better logging, fixes MNIST non-determinism

---
+++ run_experiments_grad_reg.sh
@@ -0,0 +1,19 @@
+for seed in 1 2 3 4 5 6 7 8 9 10
+do
+	for delta in 0.0 #0.01 0.05 0.1 0.2 0.5
+	do
+		# CUDA_VISIBLE_DEVICES=1 python3 -W ignore main.py --train_algo grad_reg --data house --epoch_classifier 15 --epoch_transform 5 --use_cuda --bs 1000 --delta $delta --seed $seed --early_stopping> temp_log;
+		# CUDA_VISIBLE_DEVICES=1 python3 -W ignore main.py --model baseline  --data house --epoch_classifier 25  --epoch_finetune 10  --bs 1000  --seed $seed --use_cuda --early_stopping 
+		# CUDA_VISIBLE_DEVICES=1 python3 -W ignore main.py --model tbaseline  --data house --epoch_classifier 25  --epoch_finetune 10  --bs 1000  --seed $seed --use_cuda --early_stopping 
+		# CUDA_VISIBLE_DEVICES=1 python3 -W ignore main.py --model goodfellow  --data house --epoch_classifier 25  --epoch_finetune 10  --bs 1000  --seed $seed --use_cuda --early_stopping 
+		# CUDA_VISIBLE_DEVICES=1 python3 -W ignore main.py --model GI  --data house --epoch_classifier 25  --epoch_finetune 10  --bs 1000  --seed $seed --use_cuda --early_stopping --goodfellow
+
+		CUDA_VISIBLE_DEVICES=1 python3 -W ignore main.py --model baseline  --data mnist --epoch_classifier 80  --epoch_finetune 20  --bs 250  --seed $seed --use_cuda --early_stopping 
+		CUDA_VISIBLE_DEVICES=1 python3 -W ignore main.py --model tbaseline  --data mnist --epoch_classifier 80  --epoch_finetune 20  --bs 250  --seed $seed --use_cuda --early_stopping 
+		CUDA_VISIBLE_DEVICES=1 python3 -W ignore main.py --model goodfellow  --data mnist --epoch_classifier 80  --epoch_finetune 20  --bs 250  --seed $seed --use_cuda --early_stopping 
+		CUDA_VISIBLE_DEVICES=1 python3 -W ignore main.py --model GI  --data mnist --epoch_classifier 80  --epoch_finetune 20  --bs 250  --seed $seed --use_cuda --early_stopping --goodfellow		
+		# echo $seed $delta | tee -a results_grad.txt;
+		# grep "MAE:" temp_log | tee -a results_grad.txt;
+		# grep "MSE:" temp_log | tee -a results_grad.txt;
+	done;
+done;
